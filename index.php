de233d3ad4d9c1923bf28b99d8a7ae37188aa085
julien lachaux <lachauxwebdev@gmail.com>
2018-09-07 16:55:48 +0200

    ajout test pour mustache

--- index.php
+++ index.php
@@ -8,20 +8,5 @@ $MyUser = $user->findById(1);
 // on recupère les informations sur un user (on hydrate le user)
 $user->hydrate(1);
 
-echo $user->getEmail() . "<br>";
-echo $user->getPassword() . "<br>";
-echo $user->getLastname() . "<br>";
-echo $user->getFirstname() . "<br>";
-echo $user->getAge() . "<br>";
-echo $user->getGender() . "<br>";
-
-// on recupere tout les hommes
-
-
-echo "<br><br><b>LES HOMMES DANS LA BASE</b><br><br>";
-
-$req = $db->query("SELECT id FROM users WHERE gender='male'");
-while ($data = $req->fetchObject()) {
-    $user->hydrate($data->id);
-    echo "{$user->getFirstname()} {$user->getLastname()} <br>";
-}
+$m = new Mustache_Engine;
+echo $m->render('<div>Hello {{planet}}</div>', array('planet' => 'World!')); // "Hello World!"
