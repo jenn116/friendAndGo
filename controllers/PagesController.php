e0018bdcc2b4d9322d82c628af883b969b00e948
julien-Lachaux <lachauxwebdev@gmail.com>
2018-09-27 02:51:48 +0200

    dataset: accueil

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -3,14 +3,17 @@
 class PagesController extends Controller {
 
     public function index($urlParam) {
-        echo $this->render_view('pages/accueil', 'Accueil', []);
+        $data = [];
+        if (isset($urlParam['query']['erreur'])) { $data["erreur"] = $urlParam['query']['erreur']; }
+        if (isset($_SESSION["user"])) { $data["currentUser"] = $_SESSION["user"]; }
+
+        echo $this->render_view('pages/accueil', 'Accueil', $data);
     }
 
     public function connexion($urlParam) {
         $data = [];
-        if (isset($urlParam['query']['erreur'])) {
-            $data["erreur"] = $urlParam['query']['erreur'];
-        }
+        if (isset($urlParam['query']['erreur'])) { $data["erreur"] = $urlParam['query']['erreur']; }
+
         if (isset($_SESSION["user"])) {
             header("Location: /events");
         } else {
@@ -20,17 +23,17 @@ class PagesController extends Controller {
 
     public function inscription($urlParam) {
         $data = [];
-        if (isset($urlParam['query']['erreur'])) {
-            $data["erreur"] = $urlParam['query']['erreur'];
-        }
+        if (isset($urlParam['query']['erreur'])) { $data["erreur"] = $urlParam['query']['erreur']; }
+        if (isset($_SESSION["user"])) { $data["currentUser"] = $_SESSION["user"]; }
+
         echo $this->render_view('pages/inscription', 'Accueil', $data);
     }
 
     public function activity($urlParam) {
         $data = [];
-        if (isset($urlParam['query']['erreur'])) {
-            $data["erreur"] = $urlParam['query']['erreur'];
-        }
+        if (isset($urlParam['query']['erreur'])) { $data["erreur"] = $urlParam['query']['erreur']; }
+        if (isset($_SESSION["user"])) { $data["currentUser"] = $_SESSION["user"]; }
+        
         echo $this->render_view('pages/activity', 'Accueil', $data);
     }
 
